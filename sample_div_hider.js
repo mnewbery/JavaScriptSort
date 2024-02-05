fda6cf30ab20f3f300a1ecb9b06103593d3274ea
mnewbery <mnewbery@hotmail.com>
2024-02-05 17:34:13 +1100

    Update sample_div_hider.js
    new demo of search by class

--- sample_div_hider.js
+++ sample_div_hider.js
@@ -1,6 +1,18 @@
-// This function demonstrates how to find divs by name then hide the ones that don't have the needed div_id
 function toggle_div(div_name, div_id){
-    var collection = document.getElementsByName(div_name);
+    let collection = document.getElementsByName(div_name);
+    for(let node in collection) {
+        if (collection[node] !== null && node.length < 2){
+            if (collection[node].attributes['id'].nodeValue === div_id) { // could have said item.hidden = item.id === div_id) ? false : true;
+                collection[node].style.display = 'block';
+            } else {
+                collection[node].style.display = 'none';
+            }
+        }
+    }
+}
+// Same function as above except it searches by class not element by name
+function toggle_div_byClass(div_class, div_id){
+    let collection = document.getElementsByClassName(div_class);
     for(let node in collection) {
         if (collection[node] !== null && node.length < 2){
             if (collection[node].attributes['id'].nodeValue === div_id) { // could have said item.hidden = item.id === div_id) ? false : true;
